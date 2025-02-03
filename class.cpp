77bdbc218830ebbd4a2af698016c59340574b2f1
danish dk <mohmdanish0123@gmail.com>
2025-02-03 10:27:14 +0530

    finding the size of the class

---
+++ class.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class hero{
+  int helth; 
+  string name;
+  char id;
+  bool gender;
+};
+
+
+int main(){
+// hero h1;
+// cout<<"size: "<<sizeof(h1)<<endl; 
+cout<<" size of the class "<<sizeof(hero)<<endl;
+    return 0;
+}
